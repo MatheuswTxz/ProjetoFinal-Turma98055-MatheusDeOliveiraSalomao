a2bd2ddbf6ae4d9437d0a93295944d865b912b8a
MatheuswTxz <seuemail@example.com>
2025-10-10 19:03:09 -0300

    alteração de icones no carrossel

--- src/app/components/sobre-nos/sobre-nos.component.ts
+++ src/app/components/sobre-nos/sobre-nos.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sobre-nos',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sobre-nos.component.html',
   styleUrl: './sobre-nos.component.css'
 })
 export class SobreNosComponent {
   equipe = [
-    { nome: 'Maria', cargo: 'Coordenadora', emoji: '👩‍💼' },
-    { nome: 'João', cargo: 'Voluntário', emoji: '👨‍💼' },
-    { nome: 'Ana', cargo: 'Assistente Social', emoji: '👩‍⚕️' }
+    { nome: 'Maria', cargo: 'Coordenadora', icon: 'fas fa-user-tie' },
+    { nome: 'João', cargo: 'Voluntário', icon: 'fas fa-user-tie' },
+    { nome: 'Ana', cargo: 'Assistente Social', icon: 'fas fa-user-md' }
   ];
 }
